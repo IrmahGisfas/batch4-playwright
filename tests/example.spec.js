a177e50989aa1357a2a0723dc1af2d2395c7cd2f
IrmahGisfas <irmagispa@gmail.com>
2025-02-01 11:44:20 +0700

    irmah commit

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,14 +1,14 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('ini judul satu irma', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('ini judul dua', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
